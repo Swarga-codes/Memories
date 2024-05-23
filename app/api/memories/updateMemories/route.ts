bb374a7d1eaf0ad450e6a7e0f9e33437701ee040
Swarga-codes <sd3526@srmist.edu.in>
2024-05-24 03:21:55 +0530

    created update memory api

---
+++ app/api/memories/updateMemories/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { checkValidityOfToken } from "@/app/util/checkValidityOfToken";
+import USER from "@/app/models/userSchema";
+import connectDb from "@/app/util/connectDb";
+import MEMORY from "@/app/models/memorySchema";
+export async function PUT(req:NextRequest){
+try {
+    const decodedToken = checkValidityOfToken(req);
+    if (!decodedToken.success) return NextResponse.json(decodedToken, { status: 401 });
+    const email = decodedToken?.email;
+    await connectDb();
+    const isExistingUser=await USER.findOne({email:email})
+    if(!isExistingUser) return NextResponse.json({success:false,message:'User not found!'},{status:404})
+    const {title,description,memoryCoverPic,memoryParticipants,memoryId}=await req.json()
+    memoryParticipants.push(isExistingUser?._id)
+    if(!title || !memoryId) return NextResponse.json({success:false,message:'One or more fields are missing'},{status:422})
+    const isValidMemory= await MEMORY.findOne({_id:memoryId})
+    if(!isValidMemory) return NextResponse.json({success:false,message:'Memory not found!'},{status:404})
+    if((!isValidMemory.createdBy.equals(isExistingUser._id))) return NextResponse.json({success:false,message:'User not allowed to modify or access this memory!'},{status:403})
+    isValidMemory.title=title
+    isValidMemory.description=description
+    isValidMemory.memoryCoverPic=memoryCoverPic
+    isValidMemory.memoryParticipants=memoryParticipants
+    await isValidMemory.save()
+    return NextResponse.json({success:true,message:'Updated memory successfully!'},{status:200})
+} catch (error) {
+    console.log(error)
+    return NextResponse.json({success:false,message:'Could not update user memory'},{status:500})
+}
+}
